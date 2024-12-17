a70e14645f51f8780ab3348721c31113607da034
amani <amanie4003@gmail.com>
2024-12-16 21:05:17 -0800

    Force language detection

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ print ("Welcome to the Band Name Generator !")
 user_name_city = input ("Which city did you grow up in?\n")
 user_name_pet = input("What is the name of your pet?\n")
 print ("Your new band name is: " +   user_name_city + " " +  user_name_pet)
+
+//
